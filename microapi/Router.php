aa83d7defa72450a848ee0a8de92f75b6602b481
Tom Lerendu <tom.lerendu@gmail.com>
2013-07-19 13:14:25 +0100

    More flexible wildcards in Router class

--- microapi/Router.php
+++ microapi/Router.php
@@ -30,7 +30,7 @@ class Router
 		if(!$foundMatch)
 		{
 			//TODO implement proper 404 and default 404 behaviour in config file
-			Response::getInstance()->make(["error"=>"404"]);
+			Response::getInstance()->error(404, []);
 		}
 	}
 
@@ -41,10 +41,6 @@ class Router
 	 */
 	public function matchPath($route, $path)
 	{
-		/*
-		 TODO: Be able to match /SomeRandomCharacters(?)SomeMoreRandomCharacters/ as well as /(?)/
-		*/
-
 		$route = explode('/', trim($route, '/'));
 		$path = explode('/', trim($path, '/'));
 
@@ -52,18 +48,32 @@ class Router
 		if(count($route) !== count($path))
 			return false;
 
+		//For each route and path
 		for($i=0; $i<count($route); $i++)
 		{
-			//Find out if there's a wildcard in the sectio
+			//Find out if there's a wildcard in the section
 			$wildcard = strpos($route[$i], '(?)');
 
-			//If there's is
-			if($wildcard !== false)
+			//If there is and its on its own
+			if($wildcard !== false && strlen($route[$i]) === 3)
 			{
-				if($route[$i] != '(?)')
-					return false;
-
 				$params[] = $path[$i];
+			}
+			//If there is and its bordered by other characters 
+			else if($wildcard !== false)
+			{
+				if(
+					substr($route[$i], 0, $wildcard) == substr($path[$i], 0, $wildcard) &&
+					substr($path[$i], -strlen(substr($route[$i], $wildcard+3))) == substr($route[$i], $wildcard+3)
+				)
+				{
+					$routeParts = explode('(?)', $route[$i]);
+					$param = substr($path[$i], strlen($routeParts[0]));
+					$param = substr($param, 0, -strlen($routeParts[1]));
+					$params[] = $param;
+				}
+				else if ($route[$i] != '(?)')
+					return false;
 			}
 			//If there's no wildcard make sure the route and path section match
 			else if($route[$i] != $path[$i])
