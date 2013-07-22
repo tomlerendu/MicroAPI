78b3baa5414b60ad4ba342a7a263ba6c635c1e0f
Tom Lerendu <tom.lerendu@gmail.com>
2013-07-22 20:05:33 +0100

    Support for error codes and cache control

--- vendor/microapi/Response.php
+++ vendor/microapi/Response.php
@@ -5,9 +5,18 @@ class Response extends Singleton
 {
 	public function make($responseArray, $options = [])
 	{
-		//Set the header
+		//Possible options..
+
 		if(isset($options['header']))
 			header($options['header']);
+
+		if(isset($options['cache']))
+		{
+			$ttl = gmdate('D, d M Y H:i:s', time() + $options['cache']) . ' GMT';
+			header('Expires: ' . $ttl);
+			header('Pragma: cache');
+			header('Cache-Control: max-age=' . $options['cache']);
+		}
 
 		//Figure out what format should be used
 		$format = (isset($options['format'])) ? $options['format'] : $this->config['format'];
@@ -15,15 +24,37 @@ class Response extends Singleton
 		call_user_func_array([$this, $format], [$responseArray]);
 	}
 
-	public function error($errorNum, $responseArray, $options = [])
+	public function error($error, $responseArray, $options = [])
 	{
-		switch($errorNum) 
+		//If the error was given as a number
+		if(is_numeric($error))
 		{
-			case 404:
-				header('HTTP/1.0 404 Not Found');
-				break;
+			switch($error) 
+			{
+				case 400:
+					header('HTTP/1.0 400 Bad Request');
+					break;
+				case 401:
+					header('HTTP/1.0 401 Unauthorized');
+					break;
+				case 404:
+					header('HTTP/1.0 404 Not Found');
+					break;
+				case 500:
+					header('HTTP/1.0 500 Internal Server Error');
+					break;
+				case 502:
+					header('HTTP/1.0 502 Bad Gateway');
+					break;
+				case 503:
+					header('HTTP/1.0 503 Service Unavailable');
+					break;
+			}
 		}
+		else
+			header($error);
 
+		//Make the response
 		$this->make($responseArray, $options);
 	}
 
