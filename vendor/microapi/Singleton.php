241f59714f27adfe7d29c7cc6e3db48509c886f4
Tom Lerendu <tom.lerendu@gmail.com>
2013-08-19 20:42:58 +0100

    Documentation of methods

--- vendor/microapi/Singleton.php
+++ vendor/microapi/Singleton.php
@@ -6,6 +6,11 @@ abstract class Singleton
 	private static $instance = array();
 	protected $config;
 
+	/**
+	 * Returns the only instance of the object and creates one if it doesn't already exist
+	 * 
+	 * @return The only instance of the object
+	 */
 	final public static function getInstance()
 	{
 		$calledFrom = get_called_class();
@@ -16,11 +21,17 @@ abstract class Singleton
 		return self::$instance[$calledFrom];
 	}
 
+	/**
+	 * Passes configuration from the config file into a class
+	 */
 	public function setConfig($config)
 	{
 		$this->config = $config;
 	}
 	
+	/**
+	 *
+	 */
 	protected function __construct()
 	{
 
