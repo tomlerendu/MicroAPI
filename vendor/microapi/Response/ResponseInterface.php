ce9c62f9f684c37c2e47730866423e5e0bfe1007
Tom Lerendu <tom.lerendu@gmail.com>
2013-08-20 21:23:26 +0100

    Documentation for ResponseInterface

--- vendor/microapi/Response/ResponseInterface.php
+++ vendor/microapi/Response/ResponseInterface.php
@@ -3,7 +3,22 @@ namespace MicroAPI\Response;
 
 interface ResponseInterface
 {
+	/**
+	 * 
+	 */
 	public function __construct($responseArray);
+	
+	/**
+	 * An array of headers that will be sent with the response
+	 *
+	 * @return The headers that will be sent with the response
+	 */
 	public function getHeaders();
+	
+	/**
+	 * The response in the form of a string
+	 *
+	 * @return The response string
+	 */
 	public function getResponse();
 }
