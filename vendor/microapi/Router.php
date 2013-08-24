d7a80236b362948f5980fcb595130f2eb3dcbd71
Tom Lerendu <tom.lerendu@gmail.com>
2013-08-24 16:09:47 +0100

    Support for routing based on client IP

--- vendor/microapi/Router.php
+++ vendor/microapi/Router.php
@@ -7,7 +7,7 @@ class Router
 	{
 		$request = Request::getInstance();
 		$match = false;
-
+		
 		//For each controller
 		foreach($routes as $controller => $rules)
 		{
@@ -47,16 +47,18 @@ class Router
 
 		foreach($rules as $ruleType => $ruleValue)
 		{
-			$rulePassed;
+			$rulePassed = false;
 
 			switch($ruleType) 
 			{
 				case 'path':
-					$rulePassed = $this->matchPath($ruleValue, $request->getPath());
+					$rulePassed = $this->matchWildcards($ruleValue, $request->getPath(), '/');
 					break;
 				case 'method':
 					$rulePassed = $this->matchMethod($ruleValue, $request->getMethod());
 					break;
+				case 'ip':
+					$rulePassed = $this->matchIp($ruleValue, $request->getIpAddress());
 			}
 
 			//If the rule failed
@@ -76,8 +78,35 @@ class Router
 	}
 
 	/**
+	 * 
 	 *
-	 *
+	 * @param $ip
+	 * @param $requestIp
+	 * 
+	 * @return The values of the wildcards if the method is matched, false if it doesn't
+	 */
+	private function matchIp($ip, $requestIp)
+	{
+		$ipV4 = (stripos($ip, ':') === false);
+		$requestIpV4 = (stripos($requestIp, ':') === false);
+
+		//If one is ip v4 and one is v6
+		if($ipV4 !== $requestIpV4)
+			return false;
+		//If they are both ip v4
+		else if($ipV4)
+			return $this->matchWildcards($ip, $requestIp, '.');
+		//If they are both ip v6
+		else
+		{
+			$ip = inet_ntop(inet_pton($ip));
+			$requestIp = inet_ntop(inet_pton($requestIp));
+			return $this->matchWildcards($ip, $requestIp, ':');
+		}
+	}
+
+	/**
+	 * 
 	 */
 	private function matchMethod($method, $requestMethod)
 	{
@@ -90,51 +119,54 @@ class Router
 	/**
 	 * Checks if a predefined path matches the requested path
 	 *
-	 * @param $path
+	 * @param $path 
 	 * @param $requestPath
 	 *
 	 * @return The values of the wildcards if the path is matched, false if it doesn't
 	 */
-	private function matchPath($path, $requestPath)
+	private function matchWildcards($pattern, $subject, $delimiter = false)
 	{
-		$path = explode('/', trim($path, '/'));
-		$requestPath = explode('/', trim($requestPath, '/'));
-		
+		if($delimiter !== false)
+		{
+			$pattern = explode($delimiter, trim($pattern, $delimiter));
+			$subject = explode($delimiter, trim($subject, $delimiter));
+		}
+
 		$params = [];
 
 		//If the route and path don't have the same number of sections
-		if(count($path) !== count($requestPath))
+		if(count($pattern) !== count($subject))
 			return false;
 
 		//For each route and path
-		for($i=0; $i<count($path); $i++)
+		for($i=0; $i<count($pattern); $i++)
 		{
 			//Find out if there's a wildcard in the section
-			$wildcard = strpos($path[$i], '(?)');
+			$wildcard = strpos($pattern[$i], '(?)');
 
 			//If there is and its on its own
-			if($wildcard !== false && strlen($path[$i]) === 3)
+			if($wildcard !== false && strlen($pattern[$i]) === 3)
 			{
-				$params[] = $requestPath[$i];
+				$params[] = $subject[$i];
 			}
 			//If there is and its bordered by other characters 
 			else if($wildcard !== false)
 			{
 				if(
-					substr($path[$i], 0, $wildcard) == substr($requestPath[$i], 0, $wildcard) &&
-					substr($requestPath[$i], -strlen(substr($path[$i], $wildcard+3))) == substr($path[$i], $wildcard+3)
+					substr($pattern[$i], 0, $wildcard) == substr($subject[$i], 0, $wildcard) &&
+					substr($subject[$i], -strlen(substr($pattern[$i], $wildcard+3))) == substr($pattern[$i], $wildcard+3)
 				)
 				{
-					$routeParts = explode('(?)', $path[$i]);
-					$param = substr($requestPath[$i], strlen($routeParts[0]));
+					$routeParts = explode('(?)', $pattern[$i]);
+					$param = substr($subject[$i], strlen($routeParts[0]));
 					$param = substr($param, 0, -strlen($routeParts[1]));
 					$params[] = $param;
 				}
-				else if ($path[$i] != '(?)')
+				else if ($pattern[$i] != '(?)')
 					return false;
 			}
 			//If there's no wildcard make sure the route and path section match
-			else if($path[$i] != $requestPath[$i])
+			else if($pattern[$i] != $subject[$i])
 				return false;
 		}
 
