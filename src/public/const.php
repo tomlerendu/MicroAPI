e7dc33f867ff3bdbeca27850e9f61c72f9754eb7
Tom Lerendu <Tom@Toms-MacBook-Pro.local>
2014-06-17 19:30:58 +0100

    Move Constant paths out of index.php

--- src/public/const.php
+++ src/public/const.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tom
- * Date: 17/06/2014
- * Time: 18:52
- */ 
+
+/*
+	Path to the directory containing the App and MicroAPI directories.
+
+	The only things dependant on MAIN_PATH are APP_PATH and MICROAPI_PATH
+	so it can be removed if you decide to go for an alternate directory
+	structure.
+*/
+define('MAIN_PATH', realpath(dirname(dirname(__FILE__))));
+
+/*
+	The path to the App directory
+*/
+define('APP_PATH', MAIN_PATH . DIRECTORY_SEPARATOR . 'App');
+
+/*
+	The path to the vendor directory
+*/
+define('MICROAPI_PATH', MAIN_PATH . DIRECTORY_SEPARATOR . 'MicroAPI');
+
--- src/public/index.php
+++ src/public/index.php
@@ -1,28 +1,5 @@
 <?php
 
-/*
-                    MICRO API     development version
-                    ---------------------------------
-*/
-
-/*
-	Path to the directory containing the App and MicroAPI directories.
-
-	The only things dependant on MAIN_PATH are APP_PATH and MICROAPI_PATH
-	so it can be removed if you decide to go for an alternate directory 
-	structure.
-*/ 
-define('MAIN_PATH', realpath(dirname(dirname(__FILE__))));
-
-/*
-	The path to the App directory
-*/
-define('APP_PATH', MAIN_PATH . DIRECTORY_SEPARATOR . 'App');
-
-/*
-	The path to the vendor directory
-*/
-define('MICROAPI_PATH', MAIN_PATH . DIRECTORY_SEPARATOR . 'vendor/MicroAPI');
-
+require 'const.php';
 
 require MICROAPI_PATH . DIRECTORY_SEPARATOR . '_start.php';
