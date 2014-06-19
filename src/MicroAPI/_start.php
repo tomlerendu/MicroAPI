bcdb5525f016f4b1db3a5eba175eecf47471ad33
Tom Lerendu <Tom@Toms-MacBook-Pro.local>
2014-06-20 00:24:48 +0100

    Add config and router services to the injector

--- src/MicroAPI/_start.php
+++ src/MicroAPI/_start.php
@@ -7,23 +7,28 @@ $autoloader = new \MicroAPI\Autoloader();
 $autoloader->addNamespace('App', APP_PATH);
 $autoloader->addNamespace('MicroAPI', MICROAPI_PATH);
 
-// Load the config file
-
-$config = require APP_PATH . DIRECTORY_SEPARATOR . 'config.php';
-
-// Add namespaces to the autoloader
-
-$autoloader->addNamespaces($config['autoloader']);
-
 // Create the dependency injector
 
 $injector = new \MicroAPI\Injector();
+$config = new \MicroAPI\Config();
 $injector->addDependency('injector', $injector);
-$injector->addDependency('database', '\MicroAPI\Database', $config['database']);
-$injector->addDependency('request', '\MicroAPI\Request', ['subDirectory' => $config['subDirectory']]);
-$injector->addDependency('response', '\MicroAPI\Response', $config['response']);
+$injector->addDependency('config', $config);
+
+// Run the app defined config function
+
+$injector->inject(require(APP_PATH . DIRECTORY_SEPARATOR . 'config.php'));
+
+// Add all required dependencies
+
+$injector->addDependency('database', '\MicroAPI\Database', $config->get('microapi.database'));
+$injector->addDependency('request', '\MicroAPI\Request', ['subDirectory' => $config->get('microapi.subDirectory')]);
+$injector->addDependency('response', '\MicroAPI\Response', $config->get('microapi.response'));
+$injector->addDependency('router', '\MicroAPI\Router', ['injector' => $injector]);
+
+// Run the app defined setup function
+
+$injector->inject(require(APP_PATH . DIRECTORY_SEPARATOR . 'run.php'));
 
 // Create the router and load the App routes file
 
-$router = new \MicroAPI\Router($injector);
-require APP_PATH . DIRECTORY_SEPARATOR . 'routes.php';
+$injector->inject(require(APP_PATH . DIRECTORY_SEPARATOR . 'routes.php'));
