baacaae30ff9ac6379999054775753b2005f157f
Tom Lerendu <Tom@Toms-MacBook-Pro.local>
2014-06-20 00:26:24 +0100

    Move routing into a separate function

--- src/App/routes.php
+++ src/App/routes.php
@@ -1,17 +1,22 @@
 <?php
 
-$router->get([
-    'route' => '/name/(name)',
-    'object' => 'Example@getName',
-    'require' => function() {
-        return 1 + 1 == 2;
-    }
-]);
+return function($router)
+{
 
-$router->post([
-    'route' => '/name/(name)',
-    'function' => ['functionExample', 'postName'],
-    'require' => function() {
-        return 1 + 1 == 2;
-    }
-]);
+    $router->get([
+        'route' => '/name/(name)',
+        'object' => 'Example@getName',
+        'require' => function() {
+            return 1 + 1 == 2;
+        }
+    ]);
+
+    $router->post([
+        'route' => '/name/(name)',
+        'function' => ['functionExample', 'postName'],
+        'require' => function() {
+            return 1 + 1 == 2;
+        }
+    ]);
+
+};
