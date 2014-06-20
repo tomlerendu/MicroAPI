eae7ea8887d8459f9d8ba4b373f085c3b6d83146
Tom Lerendu <Tom@Toms-MacBook-Pro.local>
2014-06-20 01:35:47 +0100

    Remove router as service, add autoloader

--- src/MicroAPI/_start.php
+++ src/MicroAPI/_start.php
@@ -13,6 +13,7 @@ $injector = new \MicroAPI\Injector();
 $config = new \MicroAPI\Config();
 $injector->addDependency('injector', $injector);
 $injector->addDependency('config', $config);
+$injector->addDependency('autoloader', $autoloader);
 
 // Run the app defined config function
 
@@ -23,7 +24,6 @@ $injector->inject(require(APP_PATH . DIRECTORY_SEPARATOR . 'config.php'));
 $injector->addDependency('database', '\MicroAPI\Database', $config->get('microapi.database'));
 $injector->addDependency('request', '\MicroAPI\Request', ['subDirectory' => $config->get('microapi.subDirectory')]);
 $injector->addDependency('response', '\MicroAPI\Response', $config->get('microapi.response'));
-$injector->addDependency('router', '\MicroAPI\Router', ['injector' => $injector]);
 
 // Run the app defined setup function
 
@@ -31,4 +31,6 @@ $injector->inject(require(APP_PATH . DIRECTORY_SEPARATOR . 'run.php'));
 
 // Create the router and load the App routes file
 
-$injector->inject(require(APP_PATH . DIRECTORY_SEPARATOR . 'routes.php'));
+$router = new \MicroAPI\Router($injector);
+$routeFunction = require(APP_PATH . DIRECTORY_SEPARATOR . 'routes.php');
+$routeFunction($router);
