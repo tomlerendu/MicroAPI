6044850bc412c1b0739fdcb894249d9e1ccd5339
Tom Lerendu <Tom@Toms-MacBook-Pro.local>
2014-06-20 16:43:51 +0100

    Simplify config

--- src/App/config.php
+++ src/App/config.php
@@ -2,36 +2,20 @@
 
 return function($config, $injector)
 {
-    /*
-        Sub directory
-        -------------
 
-        If the app is not at the root of the domain specify the sub directory it is located in.
-        EG: for website.com/api/public would be /api/public.
-        For root leave empty.
-     */
+    //Sub directory - If the public directory is not at the domain root set the sub directory it is in here
     $config->set('microapi.subDirectory', '/microapi/src/public');
 
-    /*
-		Database
-		--------
-
-		Currently only supports MYSQL
-	*/
+    //Database
     $config->set('microapi.database.user', 'root');
     $config->set('microapi.database.pass', '');
     $config->set('microapi.database.name', '');
     $config->set('microapi.database.host', 'localhost');
     $config->set('microapi.database.fetch', PDO::FETCH_ASSOC);
 
-    /*
-		Response
-		--------
-
-		The default response from the server if one wasn't defined in the response
-		method call.
-	*/
+    //Response - The default values for a response if one wasn't defined in the call Response::make()
     $config->set('microapi.response.format', '\MicroAPI\Response\JsonResponse');
-    $config->set('microapi.response.cacheTime', '0');
+    $config->set('microapi.response.cacheTime', 0);
     $config->set('microapi.response.headers', []);
+
 };
