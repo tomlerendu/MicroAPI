67dd498564b948c458491fca8bfcf0da500a7f40
Tom Lerendu <tom.lerendu@gmail.com>
2014-06-23 22:49:21 +0100

    Add a MicroAPI exception class
    Allow for better unit testing in future

---
+++ src/MicroAPI/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MicroAPI;
+
+
+class Exception extends \Exception
+{
+    public function __construct($message, $code=0, Exception $previous=null)
+    {
+        $message = 'MicroAPI: ' . $message;
+        parent::__construct($message, $code, $previous);
+    }
+} 
