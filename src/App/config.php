a182c2577fb82b1cef7a3783fdcc6bfc5124b877
Tom Lerendu <tom.lerendu@gmail.com>
2014-06-23 23:42:20 +0100

    Added comment about using the injector

--- src/App/config.php
+++ src/App/config.php
@@ -7,14 +7,17 @@ return function($config, $injector)
     $config->set('microapi.subDirectory', '/microapi/src/public');
 
     //Database
-    $config->set('microapi.database.dsn', 'mysql:host=127.0.0.1;dbname=startana;port=3306');
+    $config->set('microapi.database.dsn', 'mysql:host=127.0.0.1;dbname=;port=3306');
     $config->set('microapi.database.user', 'root');
     $config->set('microapi.database.pass', 'root');
     $config->set('microapi.database.fetchMode', PDO::FETCH_ASSOC);
 
-    //Response - The default values for a response if one wasn't defined in the call Response::make()
+    //Response - The default values for a response if they are not defined in the call Response::make()
     $config->set('microapi.response.format', '\MicroAPI\Response\JsonResponse');
     $config->set('microapi.response.cacheTime', 0);
     $config->set('microapi.response.headers', []);
+
+    //Injector - Use the injector to inject any extra services here
+
 
 };
