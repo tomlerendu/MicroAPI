a89316626a5da65073e3971819e41e08dcd26dc0
Tom Lerendu <tom.lerendu@gmail.com>
2015-01-01 15:24:01 +0000

    Remove unnecessary file

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'src/public/const.php';
-
-require MICROAPI_PATH . '/Autoloader.php';
-
-$autoloader = new \MicroAPI\Autoloader();
-$autoloader->addNamespace('App', APP_PATH);
-$autoloader->addNamespace('MicroAPI', MICROAPI_PATH);
