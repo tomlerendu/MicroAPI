8a1880155814f279dd1f03da05ee0a1d8aebb2b1
Tom Lerendu <tom.lerendu@gmail.com>
2016-06-30 15:47:55 +0100

    Replace model with Injectable

---
+++ src/MicroAPI/Injector/Injectable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TomLerendu\MicroAPI\Injector;
+
+/**
+ * A trait to inject services into an object
+ *
+ * Class Injectable
+ * @package TomLerendu\MicroAPI
+ */
+trait Injectable
+{
+
+} 
