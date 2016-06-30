e13c8e6811ae0f19680f643577741647a4933d30
Tom Lerendu <tom.lerendu@gmail.com>
2016-06-30 15:16:45 +0100

    Use spaces instead of tab

--- src/App/Controller/Example.php
+++ src/App/Controller/Example.php
@@ -3,9 +3,9 @@ namespace App\Controller;
 
 class Example
 {
-	public function getName($request, $response, $route)
-	{
+    public function getName($request, $response, $route)
+    {
         $data = ['greeting' => 'Hi, ' . $route->getWildcard('name'), 'method' => 'You used ' . $request->getMethod()];
         $response->make($data);
-	}
+    }
 }
